d1d82cc4ba3313f517587bd1e505bd4d21f2bf53
Fiqih Alfito <fiqihalfito.dev@gmail.com>
2026-07-08 08:22:38 +0700

    feat: initialize Drizzle ORM database connection and user schema definition

---
+++ backend/src/database/connect.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/bun-sql';
+import { SQL } from "bun"
+
+const client = new SQL({
+    url: process.env.DATABASE_URL!,
+    max: 20, // settingan di source code ini lebih kecil dari max_connection di postgres engine. max_connection di postgres engine adalah 100
+    idleTimeout: 30 // (detik) harus di-set agar connection tidak terus bertambah, ini bukan param default
+});
+
+export const db = drizzle({
+    client: client,
+    // relations: relations,
+    // logger: true
+});
