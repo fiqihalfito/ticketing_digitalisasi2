bf176ade472e4ee800665c59e241c3d4ead16357
Fiqih Alfito <fiqihalfito.dev@gmail.com>
2026-09-12 12:45:23 +0700

    feat: implement login form submission with client action and auth service

--- apps/frontend/app/components/login-form.tsx
+++ apps/frontend/app/components/login-form.tsx
@@ -14,6 +14,7 @@ import {
   FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { Form } from "react-router";
 
 export function LoginForm({
   className,
@@ -29,13 +30,14 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <Form method="post">
             <FieldGroup>
               <Field>
                 <FieldLabel htmlFor="email">Email</FieldLabel>
                 <Input
                   id="email"
                   type="email"
+                  name="email"
                   placeholder="m@example.com"
                   required
                 />
@@ -50,19 +52,16 @@ export function LoginForm({
                     Forgot your password?
                   </a>
                 </div>
-                <Input id="password" type="password" required />
+                <Input id="password" type="password" name="password" required />
               </Field>
               <Field>
                 <Button type="submit">Login</Button>
-                <Button variant="outline" type="button">
-                  Login with Google
-                </Button>
                 <FieldDescription className="text-center">
                   Don&apos;t have an account? <a href="#">Sign up</a>
                 </FieldDescription>
               </Field>
             </FieldGroup>
-          </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
--- apps/frontend/app/file-routes/_auth.login.tsx
+++ apps/frontend/app/file-routes/_auth.login.tsx
@@ -1,6 +1,14 @@
 import { LoginForm } from "@/components/login-form";
 import type { Route } from "./+types/_auth.login";
 
+export async function clientAction({ request }: Route.ClientActionArgs) {
+  let formData = await request.formData();
+  let email = formData.get("email") as string;
+  let password = formData.get("password") as string;
+
+  console.log(email, password);
+}
+
 export default function LoginRoute({}: Route.ComponentProps) {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
