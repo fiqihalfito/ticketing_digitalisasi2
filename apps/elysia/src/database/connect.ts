8a6ce92d2fcd39d52b87e9860b172076a5f0949a
Fiqih Alfito <fiqihalfito.dev@gmail.com>
2026-09-12 12:44:29 +0700

    feat: implement login form submission with clientAction and auth service

--- apps/elysia/src/database/connect.ts
+++ apps/elysia/src/database/connect.ts
@@ -1,15 +1,8 @@
 import { drizzle } from 'drizzle-orm/bun-sql';
 import { relations } from './relations';
 
-// const client = new SQL({
-//     url: Bun.env.DATABASE_URL!,
-//     max: 20, // settingan di source code ini lebih kecil dari max_connection di postgres engine. max_connection di postgres engine adalah 100
-//     idleTimeout: 30 // (detik) harus di-set agar connection tidak terus bertambah, ini bukan param default
-// });
-
 export const db = drizzle({
-    // client: client,
-    relations: relations,
+    relations: { ...relations },
     connection: {
         url: Bun.env.DATABASE_URL!,
         max: 20,
