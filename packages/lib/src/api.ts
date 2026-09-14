b63087ae827aa220cdfe625389be3f75af5eab80
Fiqih Alfito <fiqihalfito.dev@gmail.com>
2026-09-14 22:07:36 +0700

    refactor: rename @ticketing/elysia package to @ticketing/backend

--- packages/lib/src/api.ts
+++ packages/lib/src/api.ts
@@ -1,4 +1,4 @@
 import { treaty } from "@elysia/eden"
-import type { App } from "@ticketing/elysia"
+import type { App } from "@ticketing/backend"
 
 export const api = treaty<App>("http://localhost:3001")
